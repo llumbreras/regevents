3197a6b0cc8b8f664829f719b5922f1a8446427e
L Lumbreras <llumbreras@gmail.com>
2013-06-02 09:16:18 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Regevents::Application.routes.draw do
 root to: 'events#index'
 get '/events' => 'events#index'
-get '/events/show/:id' => 'events#show', as: 'event'
+get '/events/:id' => 'events#show', as: 'event'
 end
