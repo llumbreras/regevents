9008646ed3e5eabb73aa352cc6e887cff3b16780
L Lumbreras <llumbreras@gmail.com>
2013-06-04 20:59:27 -0700

    Add edit page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,7 @@ class EventsController < ApplicationController
 	def show
 		@event = Event.find(params[:id])
 	end
+	def edit
+		@event = Event.find(params[:id])
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Regevents::Application.routes.draw do
 root to: 'events#index'
 get '/events' => 'events#index'
 get '/events/:id' => 'events#show', as: 'event'
+get '/events/:id/edit' => 'events#edit', as: 'edit_event'
 end
