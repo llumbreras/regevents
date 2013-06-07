813c80c36bf0e05f42942da5139196b4d104b45b
L Lumbreras <llumbreras@gmail.com>
2013-06-07 10:53:21 -0700

    Add link to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Regevents::Application.routes.draw do
 root to: 'events#index'
-get '/events' => 'events#index'
-get '/events/new' => 'events#new', as: 'new_event'
-get '/events/:id' => 'events#show', as: 'event'
-get '/events/:id/edit' => 'events#edit', as: 'edit_event'
-patch '/events/:id' => 'events#update'
+# get '/events' => 'events#index'
+# get '/events/new' => 'events#new', as: 'new_event'
+# get '/events/:id' => 'events#show', as: 'event'
+# get '/events/:id/edit' => 'events#edit', as: 'edit_event'
+# patch '/events/:id' => 'events#update'
+
+resources :events
 end
 
