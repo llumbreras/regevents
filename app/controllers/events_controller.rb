ae5533a7d4795e7e7b2b044eb4b30e856071fb4d
L Lumbreras <llumbreras@gmail.com>
2013-06-14 11:16:27 -0700

    Add alert for delete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -32,7 +32,7 @@ class EventsController < ApplicationController
 	def destroy
 		@event = Event.find(params[:id])
 		@event.destroy
-		redirect_to events_path
+		redirect_to events_path , alert: "Event removed successfully!"
 	end
 
 	private 
