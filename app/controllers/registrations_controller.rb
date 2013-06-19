b0ee02a8e89dd162c50db064e8797901b4b0f351
L Lumbreras <llumbreras@gmail.com>
2013-06-18 17:30:59 -0700

    Modify index page view

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,4 +3,8 @@ class RegistrationsController < ApplicationController
 		@event = Event.find(params[:event_id])
 		@registrations = @event.registrations
 	end
+	def show
+		@event = Event.find(params[:event_id])
+		@registration = @event.registrations.find(params[:id])
+	end
 end
